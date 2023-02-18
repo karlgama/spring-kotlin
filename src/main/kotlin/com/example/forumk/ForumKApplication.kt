ea9e51b79c20b17c40177d0bf015e031b2c72cd0
kaique <kaiquesilva49@hotmail.com>
2023-02-18 20:50:43 -0300

    init kotlin project

---
+++ src/main/kotlin/com/example/forumk/ForumKApplication.kt
@@ -0,0 +1,11 @@
+package com.example.forumk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForumKApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForumKApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/forumk/ForumKApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.forumk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumKApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
